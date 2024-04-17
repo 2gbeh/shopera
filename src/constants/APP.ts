b806cd6f5b1d7d62bf1c5050d58d8abb36ace2a0
2gbeh <tugbeh.osaretin@gmail.com>
2024-04-17 01:13:04 +0100

    update app/layout

---
+++ src/constants/APP.ts
@@ -0,0 +1,14 @@
+// import APP from "@/constants/APP";
+export default Object.freeze({
+  url: "https://shopera.vercel.app",
+  name: "Shopera",
+  slug: "shopera",
+  title: "%s | Shopera",
+  summary: "Peddle Technologies - Full-Stack Developer Assessment",
+  description: "Peddle Technologies - Full-Stack Developer Assessment",
+  owner: "HWP Labs",
+  version: "1.1.0",
+  build: "WE20240417T010000",
+});
+
+export const COLOR = Object.freeze({ main: "#2370f9", accent: "#0f326c" });
